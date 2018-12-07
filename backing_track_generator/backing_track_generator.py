58ac89b3e0e79702bcb95178b63421a5bcb0f190
ItsGiusto <giustobgood@optonline.net>
2018-12-07 12:36:54 -0500

    no subscript

--- backing_track_generator/backing_track_generator.py
+++ backing_track_generator/backing_track_generator.py
@@ -23,11 +23,11 @@ class BackingTrackGenerator(object):
             toReturn = slot.value
 
             try:
-                resolutions = slot["resolutions"]["resolutionsPerAuthority"]
+                resolutions = slot.resolutions.resolutionsPerAuthority
                 if resolutions and len(resolutions > 0):
-                    values = resolutions[0]["values"]
+                    values = resolutions[0].values.
                     if values and len(values > 0):
-                        toReturn = resolutions[0]["value"]["name"]
+                        toReturn = resolutions[0].value..name
             except KeyError:
                 pass
 
